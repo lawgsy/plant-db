7d4642124e21d036e3a5928d8f9ce26a8b1d8d42
lawgsy <lawgsy@gmail.com>
2017-06-23 18:52:00 +0200

    Update

--- src/Plants/List.elm
+++ src/Plants/List.elm
@@ -58,6 +58,20 @@ showData model =
 
         mdl =
             model.mdl
+
+        buttonHtml =
+            Button.render Msgs.Mdl
+                [ 9, 0, 0, 2 ]
+                mdl
+                [ Button.ripple
+                , Button.colored
+                , Button.accent
+                , Button.raised
+                , Button.link Routing.plantsPath
+                , Options.cs "align-right mr2 mb1"
+                , Options.onClick (Msgs.UpdateKeyword "")
+                ]
+                [ text "clear filter" ]
     in
         case response of
             RemoteData.NotAsked ->
@@ -79,80 +93,75 @@ showData model =
                     ]
 
             RemoteData.Success data ->
-                div []
-                    ([ (case keyword of
-                            "" ->
-                                text ""
-
-                            x ->
-                                span [ class "mdl-typography--headline-color-contrast mx-auto block" ]
-                                    [ Button.render Msgs.Mdl
-                                        [ 9, 0, 0, 2 ]
-                                        mdl
-                                        [ Button.ripple
-                                        , Button.colored
-                                        , Button.accent
-                                        , Button.raised
-                                        , Button.link Routing.plantsPath
-                                        , Options.cs "align-right mr2 mb1"
-                                        , Options.onClick (Msgs.UpdateKeyword "")
-                                        ]
-                                        [ text "clear filter" ]
-                                    , text ("Current filter: " ++ x)
-                                    ]
-                       )
-                     ]
-                        ++ [ Table.table [ Options.cs "mx-auto" ]
-                                -- Table.table [ Options.cs "mx-auto" ]
-                                [ Table.thead []
-                                    [ Table.tr []
-                                        -- [ Table.th [ Table.numeric ] [ text "Id" ]
-                                        -- ,
-                                        [ Table.th
-                                            [ order
-                                                |> Maybe.map Table.sorted
-                                                |> Maybe.withDefault nop
-                                            , Options.onClick Msgs.ReorderTable
-                                            ]
-                                            [ text "Name" ]
-                                          -- [ Table.th [ Table.descending, Options.onClick Msgs.ReorderTable ] [ text "Name" ]
-                                          -- [ Table.th [ Table.onClick Reorder ] [ text "Name" ]
-                                          -- , Table.descending
-                                        , Table.th [] [ text "Description" ]
-                                          -- , Table.th [] [ text "Image" ]
-                                        ]
-                                    ]
-                                , Table.tbody []
-                                    (let
-                                        filteredList =
-                                            case keyword of
-                                                "" ->
-                                                    (data.plants
-                                                        |> List.sortBy .name
-                                                    )
-
-                                                _ ->
-                                                    (data.plants
-                                                        |> filterPlants keyword
-                                                        |> List.sortBy .name
-                                                    )
-                                     in
-                                        (List.map plantRow
-                                            (case order of
-                                                Just (Table.Descending) ->
-                                                    List.reverse filteredList
-
-                                                _ ->
-                                                    filteredList
-                                            )
-                                        )
+                let
+                    plantList =
+                        case order of
+                            Just (Table.Descending) ->
+                                List.reverse
+                                    (filterSort
+                                        keyword
+                                        data.plants
                                     )
-                                ]
-                           ]
-                    )
+
+                            _ ->
+                                filterSort
+                                    keyword
+                                    data.plants
+                in
+                    div []
+                        ([ (case keyword of
+                                "" ->
+                                    text ""
+
+                                x ->
+                                    span [ class "mdl-typography--headline-color-contrast mx-auto block" ]
+                                        [ buttonHtml
+                                        , text ("Current filter: " ++ x)
+                                        ]
+                           )
+                         ]
+                            ++ [ Table.table [ Options.cs "mx-auto" ]
+                                    [ Table.thead []
+                                        [ Table.tr []
+                                            [ Table.th
+                                                [ order
+                                                    |> Maybe.map
+                                                        Table.sorted
+                                                    |> Maybe.withDefault
+                                                        nop
+                                                , Options.onClick
+                                                    Msgs.ReorderTable
+                                                ]
+                                                [ text "Name" ]
+                                            , Table.th []
+                                                [ text "Description"
+                                                ]
+                                            ]
+                                        ]
+                                    , Table.tbody []
+                                        (List.map plantRow plantList)
+                                    ]
+                               ]
+                        )
 
 
-filterPlants : String -> List { a | name : String, synonyms : List String } -> List { a | name : String, synonyms : List String }
+filterSort :
+    String
+    -> List { a | name : String, synonyms : List String }
+    -> List { a | name : String, synonyms : List String }
+filterSort keyword =
+    case keyword of
+        "" ->
+            (filterPlants keyword) >> (List.sortBy .name)
+
+        _ ->
+            (List.sortBy .name)
+
+
+filterPlants :
+    String
+    -> List { a | name : String, synonyms : List String }
+    -> List { a | name : String, synonyms : List String }
 filterPlants keyword =
     let
         pattern =
@@ -174,15 +183,15 @@ filterPlants keyword =
 
 plantRow : Plant -> Html Msg
 plantRow plant =
-    --(parseLocation plant.id)(plantPath plant.id)
-    -- Options.attribute <| onClick (text "#test")
-    Table.tr [ Options.attribute <| onClick (Msgs.NewLocation (plantPath plant.id)), Options.css "cursor" "pointer" ]
-        -- [ Table.td [ Table.numeric ] [ text (toString plant.id) ]
+    Table.tr
+        [ Options.attribute <|
+            onClick (Msgs.NewLocation (plantPath plant.id))
+        , Options.css "cursor" "pointer"
+        ]
         [ Table.td
             [ Options.cs "max-width-4", Options.css "cursor" "pointer", Table.numeric ]
             [ text plant.name ]
         , Table.td
             [ Options.cs "max-width-4", Options.css "cursor" "pointer" ]
             [ text plant.description ]
-          -- , Table.td [] [ img [ src ("assets/img/" ++ plant.img), width 200 ] [] ]
         ]
